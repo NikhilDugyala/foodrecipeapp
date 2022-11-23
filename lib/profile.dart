c3ba68110f64f4f893a6e7153edb5a4fae774cdc
Tharun Ganapathi Raman <tharun.2811@gmail.com>
2022-11-23 02:30:25 -0500

    Profile page started

--- lib/profile.dart
+++ lib/profile.dart
@@ -11,13 +11,50 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  String? user = FirebaseAuth.instance.currentUser!.email ?? FirebaseAuth.instance.currentUser!.displayName;
+  String userEmail = FirebaseAuth.instance.currentUser!.email ?? "No user email";
+  String userDisplayName = "Hello, ${FirebaseAuth.instance.currentUser!.displayName}";
+  String userPhoto = FirebaseAuth.instance.currentUser!.photoURL ?? 'https://i.stack.imgur.com/34AD2.jpg';
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBarWidget(),
-      body: Text("profile"),
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left:0, bottom: 5, right: 0, top:0),
+            child: Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(20),
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: NetworkImage(userPhoto),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left:0, bottom: 10, right: 0, top:0),
+            child: Text(userDisplayName,
+              style: const TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left:0, bottom: 10, right: 0, top:0),
+            child: Text(userEmail,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
